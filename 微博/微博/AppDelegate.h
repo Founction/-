f182e871869c428722cfcde315b94a42319812c8
李胜营 <lishengying@lishengyingdeMacBook-Air-2.local>
2016-04-21 20:20:33 +0800

    添加了素材图片

---
+++ 微博/微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微博
+//
+//  Created by 李胜营 on 16/4/21.
+//  Copyright (c) 2016年 dasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微博/微博/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微博
+//
+//  Created by 李胜营 on 16/4/21.
+//  Copyright (c) 2016年 dasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
